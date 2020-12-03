28a5793368f84518f01f913fd3e7a04a679a243f
Berezin <berezin_git@mail.ru>
2020-12-03 18:53:16 +0300

    edit dz_2_6_berezin.py

--- dz_2_6_berezin.py
+++ dz_2_6_berezin.py
@@ -1,24 +1,29 @@
 structure_product = []
-number = int(input('Какое количество товаров Вы хоттите записать? '))
 list_value_name = []
 list_value_price = []
 list_value_quantity = []
 list_value_unit = []
-for goods_index in range(number):
-    info_goods = []
-    name = input('Введите название товара: ')
-    list_value_name.append(name)
-    price = int(input('Введите цену товара: '))
-    list_value_price.append(price)
-    quantity = int(input('Введите количество товара: '))
-    list_value_quantity.append(quantity)
-    unit = input('Введите единицу измерения товара: ')
-    list_value_unit.append(unit)
-    goods = {'Название': name, 'Цена': price, 'Количество': quantity, 'Единица измерения': unit}
-    info_goods.append(goods_index + 1)
-    info_goods.append(goods)
-    info_goods_tuple = tuple(info_goods)
-    structure_product.append(info_goods_tuple)
+goods_index = 0
+while True:
+    number = input('Если хотите продолжить ввод товара, нажмите "1", в противном случае любой другой символ: ')
+    if number == "1":
+        goods_index += 1
+        info_goods = []
+        name = input('Введите название товара: ')
+        list_value_name.append(name)
+        price = int(input('Введите цену товара: '))
+        list_value_price.append(price)
+        quantity = int(input('Введите количество товара: '))
+        list_value_quantity.append(quantity)
+        unit = input('Введите единицу измерения товара: ')
+        list_value_unit.append(unit)
+        goods = {'Название': name, 'Цена': price, 'Количество': quantity, 'Единица измерения': unit}
+        info_goods.append(goods_index)
+        info_goods.append(goods)
+        info_goods_tuple = tuple(info_goods)
+        structure_product.append(info_goods_tuple)
+    else:
+        break
 
 print(structure_product)
 
